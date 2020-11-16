6d503204d490a692f4c2b31ea3eba16aef2ee465
Administrator <admin@example.com>
2020-11-16 18:44:53 +0100

    kinter

--- customerApp/src/customerApp/customer/customerAppCustomerComponent.ts
+++ customerApp/src/customerApp/customer/customerAppCustomerComponent.ts
@@ -1,68 +1,67 @@
 import { Component } from '@angular/core';
 import { baseLogger } from '../utility/logger';
-import { Customer } from "./customerApp.model" ;
-import {HttpClient} from "@angular/common/http"
+import { Customer } from "./customerApp.model";
+import { HttpClient } from "@angular/common/http"
 @Component({
 
-  templateUrl: './customerAppCustomerView.html' 
-   /** charge la vue */
+  templateUrl: './customerAppCustomerView.html'
+  /** charge la vue */
 })
-export class customerComponent { 
+export class customerComponent {
   /** export pour être importé */
-  logger : baseLogger = null;
-  disableWaitResponse : boolean = false ;
+  logger: baseLogger = null;
+  disableWaitResponse: boolean = false;
   title = 'customerApp';
-  customerModel : Customer = new Customer() ;
-  customerModels : Array<Customer> = new Array<Customer>() ;
+  customerModel: Customer = new Customer();
+  customerModels: Array<Customer> = new Array<Customer>();
 
-  constructor(loggerInject : baseLogger, public http:HttpClient)  // dependecy injection
+  constructor(loggerInject: baseLogger, public http: HttpClient)  // dependecy injection
   {
-    this.logger = loggerInject ;
+    this.logger = loggerInject;
     this.logger.log();
-    
-    
+    this.getFromServer();
   }
 
-  postToServer(){
-    var dtoCustomer:any = {} ;
-    this.disableWaitResponse = true ;
-    dtoCustomer.CustomerName  = this.customerModel.CustomerName ;
-    dtoCustomer.CustomerCode  = this.customerModel.CustomerCode ;
-    dtoCustomer.CustomerAmount  = this.customerModel.CustomerAmount ;
-    
-    this.http.post("http://localhost:3000/customers", dtoCustomer).subscribe(res=>this.success(res), res=>this.error(res)) ;
-    
+  postToServer() {
+    var dtoCustomer: any = {};
+    this.disableWaitResponse = true;
+    dtoCustomer.CustomerName = this.customerModel.CustomerName;
+    dtoCustomer.CustomerCode = this.customerModel.CustomerCode;
+    dtoCustomer.CustomerAmount = this.customerModel.CustomerAmount;
 
-  }
-  error(res){
-    console.log("erreur" + res) ;
-    console.log(res) ;
-  }
-  success(res){
-    this.getFromServer() ;
-   
-  }
-
-  getFromServer(){
-
-    this.http.get("http://localhost:3000/customers").subscribe(res=>this.successGet(res), res=>this.error(res)) ;
-  }
-  successGet(res){
-    console.log(res) ;
-    this.customerModels= res ;
-    this.disableWaitResponse = false ;
-    this.customerModel = new Customer() ;
-  }
-  
+    this.http.post("http://localhost:3000/customers", dtoCustomer).subscribe(res => this.success(res), res => this.error(res));
 
 
-  selectCustomer(_selected: Customer){
-    this.customerModel = _selected ;
+  }
+  error(res) {
+    console.log("erreur" + res);
+    console.log(res);
+  }
+  success(res) {
+    this.getFromServer();
+
   }
 
-  add(){
-    this.customerModels.push(this.customerModel) ;
+  getFromServer() {
+
+    this.http.get("http://localhost:3000/customers").subscribe(res => this.successGet(res), res => this.error(res));
+  }
+  successGet(res) {
+    console.log(res);
+    this.customerModels = res;
+    this.disableWaitResponse = false;
+    this.customerModel = new Customer();
+  }
+
+
+
+  selectCustomer(_selected: Customer) {
+    this.customerModel = _selected;
+  }
+
+  add() {
+    this.customerModels.push(this.customerModel);
     this.customerModel = new Customer()  // clear UI
-    }
- 
+  }
+
 }
